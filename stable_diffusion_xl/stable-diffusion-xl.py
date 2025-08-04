03816a687a48f364493abf63ee175113f4f81358
Erin Xu <148895066+ErinXU2004@users.noreply.github.com>
2025-08-04 14:15:19 +0800

    Update stable-diffusion-xl.py

--- stable_diffusion_xl/stable-diffusion-xl.py
+++ stable_diffusion_xl/stable-diffusion-xl.py
@@ -17,7 +17,7 @@ def main():
     from cmd_helper import optimum_cli
     import huggingface_hub as hf_hub
     from gradio_helper import make_demo_sd_xl_text2image
-    
+    from datasets import load_dataset
 
     model_dir = "/home/erinhua/openvino-genai-scripts/FLUX.1-schnell-int4-ov"
     device = "GPU"
@@ -34,19 +34,15 @@ def main():
     os.makedirs(text_dir, exist_ok=True)
 
     text_latencies = []
-    num_examples = 0
+    num_examples = 200
 
-    metadata_path = Path("/home/erinhua/metadata.parquet")
-    if not metadata_path.exists():
-        print("❌ metadata.parquet not found. Please provide it in the script directory.")
-        return
-    
-    metadata_df = pd.read_parquet(metadata_path)
-    selected_requests = metadata_df.iloc[0:num_examples].copy()
+    ds = load_dataset("lmms-lab/COCO-Caption2017", split="train")
+    selected = ds.select(range(num_examples))
 
-    for i,row in selected_requests.iterrows():
-        prompt = row['prompt']
-        clean_prompt = re.sub(r'[^\w\-_\.]', '_', prompt)[:230]
+
+    for row in selected:
+        prompt = row.get('prompt') or row['captions'][0]
+        clean_prompt = safe_filename(prompt)
         image_path = f"{text_dir}/{clean_prompt}.png"
 
         # 🟢 Create new progress bar for each request
@@ -72,52 +68,6 @@ def main():
         pbar.close()
 
 #    print(f"✅ Text2Image done. Avg latency: {sum(text_latencies)/len(text_latencies):.2f}s")
-
-   
-
-
- # ==== Image-to-Image  ====
-    image2image_pipe = ov_genai.Image2ImagePipeline(model_dir, device=device)
-    photo_prompt = "professional photo of a cat, extremely detailed, hyper realistic, best quality, full hd"
-    photo_dir = Path("./generated_image2image")
-    os.makedirs(photo_dir, exist_ok=True)
-    photo_seed = 42
-    num_steps = 35
-    def image_to_tensor(image: Image) -> ov.Tensor:
-        pic = image.convert("RGB")
-        image_data = np.array(pic.getdata()).reshape(1, pic.size[1], pic.size[0], 3).astype(np.uint8)
-        return ov.Tensor(image_data)
-
-    image2image_latencies = []
-    text_image_files = sorted(Path(text_dir).glob("*.png"))[:200]
-    for i, image_path in enumerate(text_image_files):
-        init_image = Image.open(image_path)
-        init_tensor = image_to_tensor(init_image)
-        gen = ov_genai.TorchGenerator(photo_seed + i)
-        
-        pbar = tqdm(total=num_steps)
-
-        def callback(step, total_steps, latent):
-            pbar.update(1)
-            sys.stdout.flush()
-            return False
-
-        start = time.time()
-        photo_result = image2image_pipe.generate(
-            photo_prompt,
-            image=init_tensor,
-            num_inference_steps=num_steps,
-            strength=0.75,
-            generator=gen,
-            callback=callback
-        )
-        end = time.time()
-        image2image_latencies.append(end - start)
-        out_name = image_path.stem + "_2.png"
-        photo_image = Image.fromarray(photo_result.data[0])
-        photo_image.save(photo_dir / out_name)
-        pbar.close()
-    print(f"✅ Image2Image done. Avg latency: {sum(image2image_latencies)/len(image2image_latencies):.2f}s")
 
 
 if __name__ == "__main__":
