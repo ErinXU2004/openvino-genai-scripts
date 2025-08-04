8ff402fb0904f481dd38ce9d1c992d6a6f425918
Erin Xu <148895066+ErinXU2004@users.noreply.github.com>
2025-08-04 14:08:36 +0800

    Update flux.1-image-generation.py

--- flux.1-image-generation/flux.1-image-generation.py
+++ flux.1-image-generation/flux.1-image-generation.py
@@ -16,7 +16,7 @@ def main():
     from datasets import load_dataset
 
 
-    model_dir = "OpenVINO/stable-diffusion-v1-5-fp16-ov"
+    model_dir = ""
    # model_dir = "/home/erinhua/openvino-genai-scripts/FLUX.1-schnell-int4-ov"
     # Device and pipeline
     device = "GPU"
